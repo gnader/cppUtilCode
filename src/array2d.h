1524d50519924b71d982cb8e908c9c67be7b5eeb
gnader90 <georgesnader90@gmail.com>
2021-04-22 20:54:12 +0800

    added documentation to array2d

--- src/array2d.h
+++ src/array2d.h
@@ -28,6 +28,18 @@
 
 #include <array>
 
+/**
+ * @Brief
+ * A 2d column major array class with an interface similar to std::array
+ * 
+ * example:
+ * -------
+ * array2d<float, 2, 3> arr0 {1, 2, 3, 4, 5, 6};
+ * array2d<float, 2, 3> arr2 {{1, 2, 3,}, {4, 5, 6}};
+ */
+
+//TODO : add an iterator over the elemets of a row
+
 template <typename T, size_t COL, size_t ROW>
 class array2d
 {
@@ -41,12 +53,11 @@ public:
   typedef value_type &reference;
   typedef const value_type &const_reference;
 
-  typedef std::array<value_type, ROW> column_type;
-  typedef std::array<reference, COL> row_type;
-  typedef std::array<column_type, COL> data_type;
-
   typedef value_type *pointer;
   typedef const value_type *const_pointer;
+
+  typedef std::array<value_type, ROW> column_type;
+  typedef std::array<column_type, COL> data_type;
 
   typedef typename column_type::iterator iterator;
   typedef typename column_type::const_iterator const_iterator;
@@ -82,6 +93,11 @@ public:
   //============================================
   //                Data Access
   //============================================
+  /**
+   * @Brief
+   * Returns a reference to the element at specified location {col, row}, with bounds checking.
+   * If {col, row} is not within the range of the container, an exception of type std::out_of_range is thrown.
+  **/
   reference at(size_t col, std::size_t row)
   {
     return mData.at(col).at(row);
@@ -92,6 +108,13 @@ public:
     return mData.at(col).at(row);
   }
 
+  /**
+   * @Brief
+   * Returns a reference to the element at specified location i, with bounds checking.
+   * If i is not within the range of the container, an exception of type std::out_of_range is thrown.
+   * 
+   * i is the index of the element in a column-major ordering.
+  **/
   reference at(size_t i)
   {
     std::size_t col = i / ROW;
@@ -108,6 +131,10 @@ public:
     return at(col, row);
   }
 
+  /**
+   * @Brief
+   * Returns a reference to the element at specified location {col, row}. No bounds checking is performed.
+  **/
   reference operator()(size_t col, size_t row) noexcept
   {
     return mData[col][row];
@@ -118,6 +145,12 @@ public:
     return mData[col][row];
   }
 
+  /**
+   * @Brief
+   * Returns a reference to the element at specified location i. No bounds checking is performed.
+   * 
+   * i is the index of the element in a column-major ordering.
+  **/
   const_reference operator()(size_t i) const noexcept
   {
     std::size_t col = i / ROW;
@@ -142,6 +175,10 @@ public:
     return mData[col][row];
   }
 
+  /**
+   * @Brief
+   * Returns a reference to the first element in the container.
+  **/
   reference front()
   {
     return at(0, 0);
@@ -152,6 +189,10 @@ public:
     return at(0, 0);
   }
 
+  /**
+   * @Brief
+   * Returns a reference to the last element in the container.
+  **/
   reference back()
   {
     return at(COL - 1, ROW - 1);
@@ -162,6 +203,12 @@ public:
     return at(COL - 1, ROW - 1);
   }
 
+  /**
+   * @Brief
+   * Returns pointer to the underlying array serving as element storage. 
+   * The pointer is such that range [data(); data() + size()) is always a valid range, 
+   * even if the container is empty (data() is not dereferenceable in that case).
+  **/
   pointer data()
   {
     return mData.data()->data();
@@ -172,6 +219,11 @@ public:
     return mData.data()->data();
   }
 
+  /**
+   * @Brief
+   * Returns a reference to the ith column of the 2d array, with bounds checking.
+   * If i is not within the range of the columns, an exception of type std::out_of_range is thrown.
+  **/
   column_type &col(size_t i)
   {
     return mData.at(i);
@@ -185,6 +237,10 @@ public:
   //============================================
   //                iterators
   //============================================
+  /**
+   * @Brief
+   * Returns an iterator to the first element of the array.
+  **/
   iterator begin() noexcept
   {
     return mData.front().begin();
@@ -195,6 +251,10 @@ public:
     return mData.front().begin();
   }
 
+  /**
+   * @Brief
+   * Returns an iterator to the last element of the array.
+  **/
   iterator end() noexcept
   {
     return mData.back().end();
@@ -205,6 +265,11 @@ public:
     return mData.back().end();
   }
 
+  /**
+   * @Brief
+   * Returns an iterator to the first element of the ith coloum.
+   * If i is not within the range of the columns, an exception of type std::out_of_range is thrown.
+  **/
   iterator begin_col(size_t i)
   {
     return mData.at(i).begin();
@@ -215,6 +280,14 @@ public:
     return mData.at(i).begin();
   }
 
+  /**
+   * @Brief
+   * Returns an iterator to the last element of the ith column.
+   * If i is not within the range of the columns, an exception of type std::out_of_range is thrown.
+   * 
+   * if i is the last column, than it is equal to end().
+   * if i is not the last columns, that is is the equal to begin_col(i+1)
+  **/
   iterator end_col(size_t i)
   {
     return mData.at(i).end();
@@ -228,20 +301,56 @@ public:
   //============================================
   //                capacity
   //============================================
+  /**
+   * @Brief
+   * Returns the 2d size of the array.
+  **/
   inline size_type size() const noexcept { return {COL, ROW}; }
+
+  /**
+   * @Brief
+   * Returns the size of a column.
+  **/
   inline size_t size_col() const noexcept { return ROW; }
+
+  /**
+   * @Brief
+   * Returns the size of a row.
+  **/
   inline size_t size_row() const noexcept { return COL; }
 
-  inline size_t max_size() const noexcept { return COL * ROW; }
+  /**
+   * @Brief
+   * Returns the number of elements in the 2d array.
+  **/
   inline size_t num() const noexcept { return COL * ROW; }
+  inline size_t max_size() const noexcept { return COL * ROW; }
+
+  /**
+   * @Brief
+   * Returns the number of columns.
+  **/
   inline size_t num_col() const noexcept { return COL; }
+
+  /**
+   * @Brief
+   * Returns the number of row.
+  **/
   inline size_t num_row() const noexcept { return ROW; }
 
+  /**
+   * @Brief
+   * checks whether the number of elemets is 0.
+  **/
   inline bool empty() const noexcept { return num() == 0; }
 
   //============================================
   //                operations
   //============================================
+  /**
+   * @Brief
+   * Assigns the given value value to all elements in the array.
+  **/
   void fill(const T &value)
   {
     iterator itr = begin();
@@ -250,7 +359,7 @@ public:
       *itr++ = value;
   }
 
-public:
+protected:
   data_type mData;
 };
 
